483e93b9f1c7d1049882d622e653f3d9f693e85f
Imranferozshaikh <97595297+Imranferozshaikh@users.noreply.github.com>
2024-02-14 12:07:31 +0000

    changes the program add prime number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,51 @@ public void OddEven(int n){
     System.out.println("Odd");
   }
 }
+
+  public void Stresult(int Marks){
+    if(Marks>=90){
+      System.out.println("A");
+    }
+    else if(Marks>=80){
+      System.out.println("B");
+    }
+    else if(Marks>=70){
+      System.out.println("C");
+    }
+    else if(Marks>=60){
+      System.out.println("D");
+    }
+    else{
+      System.out.println("F");
+    }
+  }
+
+  public void counting()
+  {
+    int i=1;
+   while(i<=10){
+     System.out.println(i);
+     i++;
+   }
+  }
+
+  public void isprime(int n){
+    Scanner sc =new Scanner(System.in);
+   
+
+    for(int i=2;i<n;i++){
+       int m=sc.nextInt();
+      int count=0;
+      
+
+      
+    }
+        
+      
+    }
+  }
+  
+  
   
   
 }
@@ -38,11 +83,18 @@ public class Main {
 // Odd and Even number 
     imp i = new imp();
     
-    Scanner sc = new Scanner(System.in);
-    int x = sc.nextInt();
-    i.OddEven(x);
+    // Scanner sc = new Scanner(System.in);
+    // //int x = sc.nextInt();
+    // int m =sc.nextInt();
+   // i.OddEven(x);
+   // i.Stresult(m);
+    i.counting();
      
- 
+ //*******************************************
+
+    
+
+    
     
  }
 
